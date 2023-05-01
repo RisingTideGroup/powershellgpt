34dc3bb31b5ff3ddb83effa033a9486ffc60cb30
Mendy Green <greenlighttec@users.noreply.github.com>
2023-05-01 14:56:03 -0400

    Update Start-DemoChatBot.ps1

--- Start-DemoChatBot.ps1
+++ Start-DemoChatBot.ps1
@@ -1,5 +1,5 @@
 $APIKey = '' #Place your OpenAI API Key here
-$OpenAIModel = 'gpt-3.5-turbo' #Swap this out for 'gpt-4' if you have access to it on your account
+$OpenAIModel = 'gpt-3.5-turbo'
 $Headers = @{Authorization = "Bearer $APIKey"}
 $OpenAIBaseURL = "https://api.openai.com/v1"
 
@@ -26,11 +26,17 @@ function Invoke-AIWebRequest {
 }
 
 $Instruction = @"
-You are required to build a prompt for ChatGPT to follow that will instruct it to behave in a context that makes sense for the nmame its been given.
+You are required to build a prompt for ChatGPT to follow that will instruct it to behave in a context that makes sense for the name its been given.
 For example if the name is snarkbot you want to instruction prompt to specify that the AI should be as snarky as possible. If the name indicates that the bot should be a writer then the prompt should tell it to be as eloquent and story telling as possible in its replies.
+You want to ensure that the instructions include assuming the role in its entireity, and using role play adopting the character completely. If the name indicates "Bot" then the instructions should allow for the persona to be an AI Model of the role.
 
+Example 1
 Name: Snarkbot
 Reply: You are Snarkbot, a witty and sarcastic AI. Your goal is to respond to questions and comments with the utmost snarkiness and sarcasm. Don't hold back, let your wit shine through in every response!
+
+Example 2
+Name: Albus Dumbledore
+Reply: You are Albus Dumbledore, a wise and powerful wizard. You are the headmaster of Hogwarts and your goal is to offer guidance and inspiration to those who seek your advice and enlightenment to all through education. Speak in a calm and measured tone, and offer your insight with depth and cryptic statements. As a great wizard and mentor, your words carry weight and should be chosen carefully.
 
 Name: $BotName
 "@
@@ -39,9 +45,13 @@ $AIInstruction = Invoke-AIWebRequest -Messages @(@{role="user"; content=$Instruc
 Write-Host $AIInstruction.choices.message.content -Foreground Yellow
 
 $ChatMessages = @(
-     @{
+    @{
         role="system";
-        content= $AIInstruction.choices.message.content
+        content="We are in a role playing game. Adopt the persona with 100% commitment. Provide an immersive experience in your interactions for the context provided in the role."
+    }
+     @{
+        role="user";
+        content= $AIInstruction.choices.message.content.replace("Reply: ","")
     };
 )
 $InputString = Read-Host -Prompt "$UserName"
